7594c46c55178722b3126c51da436828fc3e834a
Marco Puccio <mpuccio90@gmail.com>
2016-04-03 13:50:15 -0300

    add get_page tpltag

--- simplesite/templatetags/simplesite_tags.py
+++ simplesite/templatetags/simplesite_tags.py
@@ -19,3 +19,14 @@ def get_footer_list():
     Returns a list of pages that belong to header
     """
     return Page.objects.filter(is_footer=True)
+
+@register.assignment_tag
+def get_page(**kwargs):
+    """
+    Returns a page object filtered by the given kwargs
+    """
+    try:
+        page = Page.objects.get(**kwargs)
+    except:
+        page = None
+    return page
